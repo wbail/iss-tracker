a07055501456e2b8e219ee5c0b9477f751a3d063
Guilherme Bail <guilherme.bail@stone.com.br>
2021-04-18 16:41:52 -0300

    refactor: solution projects
    add projects in order to make the
    tests easier
    
    resolves #18

---
+++ IssTracker.Application.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IssTracker.Application.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ IssTracker.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IssTracker.Application
+{
+    public class Class1
+    {
+    }
+}
